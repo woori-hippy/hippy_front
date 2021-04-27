838f0665509ca92f3c9934ee6b24b8029bfb7ade
Hwang In Seo <sjsjsj1246@gmail.com>
2021-04-28 02:57:40 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 css={style}>Hippy</h1>
     </div>
   );
 }
+
+const style = css`
+  width: 100%;
+  text-align: center;
+`;
 
 export default App;
