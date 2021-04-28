e2688f3205397f7bc0b6c65f6b3b623c64c66308
sjsjsj1246 <sjsjsj1246@gmail.com>
2021-04-29 01:41:02 +0900

    feat add material-ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
 import "./App.css";
+import Blog from "./components/Blog";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
-      <h1 css={style}>Hippy</h1>
-    </div>
+    // <div className="App">
+    //   <h1 css={style}>Hippy</h1>
+    // </div>
+    <Blog></Blog>
   );
 }
 
