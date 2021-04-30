82555018aba28695a1e427ee7304559203d8367c
Hwang In Seo <sjsjsj1246@gmail.com>
2021-04-30 09:02:21 +0900

    feat: feature post 디자인 수정

--- src/components/FeaturedPost.jsx
+++ src/components/FeaturedPost.jsx
@@ -1,20 +1,26 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Hidden from "@material-ui/core/Hidden";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   card: {
-    display: 'flex',
+    display: "flex",
+    backgroundSize: "cover",
+    height: "15rem",
+    alignItems: "center",
   },
   cardDetails: {
     flex: 1,
+    textShadow: "0px 0px 18px rgba(0, 0, 0, 1)",
+    color: "white",
   },
   cardMedia: {
     width: 160,
@@ -27,32 +33,40 @@ function FeaturedPost(props) {
 
   return (
     <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href="#">
-        <Card className={classes.card}>
-          <div className={classes.cardDetails}>
-            <CardContent>
-              <Typography component="h2" variant="h5">
-                {post.title}
-              </Typography>
-              <Typography variant="subtitle1" color="text.secondary">
-                {post.date}
-              </Typography>
-              <Typography variant="subtitle1" paragraph>
-                {post.description}
-              </Typography>
-              <Typography variant="subtitle1" color="primary">
-                Continue reading...
-              </Typography>
-            </CardContent>
-          </div>
-          <Hidden smDown>
-            <CardMedia
-              className={classes.cardMedia}
-              image={post.image}
-              title={post.imageText}
-            />
-          </Hidden>
-        </Card>
+      <CardActionArea>
+        <Link to="/events">
+          <Card
+            className={classes.card}
+            sx={{ backgroundImage: `url(${post.image})` }}
+          >
+            <div className={classes.cardDetails}>
+              <CardContent>
+                <Typography component="h2" variant="h5">
+                  {post.title}
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  paragraph
+                  color="#8C8C8C"
+                  fontSize="0.8rem"
+                  pr={40}
+                >
+                  {post.description}
+                </Typography>
+                <Typography variant="subtitle1" color="white">
+                  Go Event
+                </Typography>
+              </CardContent>
+            </div>
+            {/* <Hidden smDown>
+              <CardMedia
+                className={classes.cardMedia}
+                image={post.image}
+                title={post.imageText}
+              />
+            </Hidden> */}
+          </Card>
+        </Link>
       </CardActionArea>
     </Grid>
   );
