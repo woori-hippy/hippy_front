068c2cc807d929a4827e984bc57cf849be90574f
Kimchohee <chchgml10@gmail.com>
2021-05-01 05:51:23 +0900

    feat nft create page

---
+++ src/container/NFTCreateContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NFTCreate from "../components/NFTCreate";
+
+const NFTCreateContainer = (props) => {
+  return <NFTCreate />;
+};
+
+export default NFTCreateContainer;
---
+++ src/pages/NFTCreatePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NFTCreateContainer from "../container/NFTCreateContainer";
+
+const NFTCreatePage = (props) => {
+  return <NFTCreateContainer />;
+};
+
+export default NFTCreatePage;
