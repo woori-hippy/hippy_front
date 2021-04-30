a6ecb76e630d496efe4e5b6b1d242cb3f31fbe90
Hwang In Seo <sjsjsj1246@gmail.com>
2021-04-30 09:16:17 +0900

    feat: product component routing

---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const Product = (props) => {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+};
+
+export default Product;
