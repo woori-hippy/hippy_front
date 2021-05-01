4681832ed08397ffa8b300000926fe3c3b964864
sjsjsj1246 <sjsjsj1246@gmail.com>
2021-05-02 06:44:59 +0900

    fix register error

--- src/container/BankAccountRegisterContainer.jsx
+++ src/container/BankAccountRegisterContainer.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import BankAccountRegister from "../components/BankAccountRegister";
 
 const BankAccountRegisterContainer = (props) => {
-  const user = useSelector((state) => state.login.user);
+  const user = useSelector((state) => state.user.user);
   return <BankAccountRegister user={user} />;
 };
 
